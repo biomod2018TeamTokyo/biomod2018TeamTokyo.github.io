49ff4f3125e65d4840253607006cd2d842e796be
biomod2018TeamTokyo <41801142+biomod2018TeamTokyo@users.noreply.github.com>
2018-08-12 22:00:49 +0900

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('a[href^=#]').click(function() {
+		var speed = 400;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var headerHeight =　170px ;  //maxであることに注意
+		var position = target.offset().top - headerHeight; //headerズレ補正
+		$('body,html').animate({scrollTop:position}, speed, 'swing');
+		return false;
+	});
+});
