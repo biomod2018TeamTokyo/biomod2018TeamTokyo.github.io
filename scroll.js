28f34b7f53e59902e58ab303ed6ab67cd6fd4ba2
biomod2018TeamTokyo <41801142+biomod2018TeamTokyo@users.noreply.github.com>
2018-08-12 22:04:45 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -3,7 +3,7 @@ $(function(){
 		var speed = 400;
 		var href= $(this).attr("href");
 		var target = $(href == "#" || href == "" ? 'html' : href);
-		var headerHeight =　170px ;  //maxであることに注意
+		var headerHeight =　 170;  //maxであることに注意
 		var position = target.offset().top - headerHeight; //headerズレ補正
 		$('body,html').animate({scrollTop:position}, speed, 'swing');
 		return false;
